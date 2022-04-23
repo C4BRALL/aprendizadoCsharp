f9c308d1a83905e94cbc69cde1ea9e7c2f561f7e
Cabral <jcabral304@gmail.com>
2022-04-23 13:39:05 -0300

    condicionais

---
+++ variaveis-numericas-alura/Variaveis-bool/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("executando");
+
+        int idade = 13;
+        int quantidadeDePessoas = 2;
+
+        bool acompanhado = quantidadeDePessoas > 1;
+
+        if (idade >= 18 || acompanhado)
+        {
+            Console.WriteLine("pode entrar");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar");
+        }
+
+        Console.WriteLine("fim da execução...");
+        Console.ReadLine();
+    }
+}
---
+++ variaveis-numericas-alura/condicionais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("executando");
+
+        int idade = 13;
+        int quantidadeDePessoas = 1;
+
+        if (idade >= 18 && quantidadeDePessoas > 1)
+        {
+            Console.WriteLine("tem a idade maior ou igual a 18 anos");
+        }
+        else
+        {
+            if (quantidadeDePessoas < 2)
+            {
+                Console.WriteLine("não está acompanhado");
+            }
+            Console.WriteLine("é menor de idade");
+        }
+
+        Console.WriteLine("fim da execução...");
+        Console.ReadLine();
+    }
+}
