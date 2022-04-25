990bbb9373d08a8f990c9469454114ab6c3650ea
Cabral <jcabral304@gmail.com>
2022-04-25 10:09:31 -0300

    iniciando C# com POO

---
+++ iniciando-com-orientacao-a-objetos/byteBank/byteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using byteBank;
+
+Console.WriteLine("Boas vindas ao seu banco, ByteBank!");
+
+ContaCorrente conta1 = new ContaCorrente();
+
+//string titular = "Andre Bessa";
+//string conta = "10123-x";
+//int numero_agencia = 23;
+//string nome_agencia = "Agencia Central";
+//double saldo = 93.50;
+
+Console.WriteLine($"titular: {titular}");
+Console.WriteLine($"conta: {conta}");
+Console.WriteLine($"Numero da Agencia: {numero_agencia}");
+Console.WriteLine($"Nome da Agência: {nome_agencia}");
+Console.WriteLine($"Saldo: {saldo}");
+
+Console.ReadKey();
