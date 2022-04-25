c0593a235960c9b4d5e46f721c08b6f9f27dfa1f
Cabral <jcabral304@gmail.com>
2022-04-25 09:42:24 -0300

    fim do curso de introdução ao C#

---
+++ variaveis-numericas-alura/extra-break/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        for (int linhas = 0; linhas < 5; linhas++)
+        {
+            for(int colunas = 0; colunas < 5; colunas++)
+            {
+                Console.Write("*");
+                if (colunas >= linhas)
+                break;
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("fim da execução...");
+        Console.ReadLine();
+    }
+}
