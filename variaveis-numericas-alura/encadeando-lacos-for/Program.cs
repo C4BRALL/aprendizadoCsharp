10b71206cb4c4b64ca51679841c3d70d7185dee9
Cabral <jcabral304@gmail.com>
2022-04-25 00:28:59 -0300

    Laços de repetições

---
+++ variaveis-numericas-alura/encadeando-lacos-for/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double investimento = 0;
+        double fatorRendimento = 1.015;
+
+        // rendimento de 0.15% ao mês
+
+        for (int anos = 0; anos <= 10; anos++)
+        {
+            for(int mes = 0; mes <= 12; mes++)
+            {
+                //Console.WriteLine($"valor do investimento por mês = {investimento}");
+                investimento = (investimento + 1000) * 1.015;
+            }
+
+            fatorRendimento += 0.001;
+
+            Console.WriteLine($"investimento no ano {anos} com o fator de rendimento = {fatorRendimento}: {investimento}");
+        }
+
+        Console.WriteLine($"valor total = {investimento}");
+
+        Console.WriteLine("fim da execução....");
+        Console.ReadLine();
+    }
+}
--- variaveis-numericas-alura/laços-de-repeticao/Program.cs
+++ variaveis-numericas-alura/laços-de-repeticao/Program.cs
@@ -4,7 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("executando");
+        double investimento = 1000;
+        int mes = 1;
+
+        // rendimento de 0.5% ao mês
+
+        // mês 1
+        //investimento = investimento + investimento * 0.005;
+        // mês 2
+        //investimento = investimento + investimento * 0.005;
+        // mês 3
+        //investimento = investimento + investimento * 0.005;
+        // mês 4
+        //investimento = investimento + investimento * 0.005;
+
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+
+            Console.WriteLine($"investimento no mês {mes}: {investimento}");
+            
+            mes ++;
+        }
+
+
+        Console.WriteLine("fim da execução....");
         Console.ReadLine();
     }
 }
